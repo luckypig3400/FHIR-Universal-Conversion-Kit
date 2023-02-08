2abd001485d62bde11e0e0db5676c8cc62149bfb
郵差 <twHenryYang@gmail.com>
2023-02-08 14:44:36 +0800

    feat: TWCR-Condition convert Original csv2json format data

--- profile/TWCR-condition.js
+++ profile/TWCR-condition.js
@@ -8,12 +8,14 @@ module.exports.profile = {
   fhirServerBaseUrl: 'https://hapi.fhir.tw/fhir',
   action: 'return', // return, upload
 }
-let trID = "TimeRandomID";
+// 此Profile的JSON結構資料參考自以下網頁:
+// https://mitw.dicom.org.tw/IG/TWCR_SF/StructureDefinition-condition-profile.html
+// 此Profile的完整JSON範例檔:
+// https://mitw.dicom.org.tw/IG/TWCR_SF/Condition-ConditionExample.json.html
+
 module.exports.globalResource = {
   // Should be resource name
   Condition: {
-    // 全域資料參考自以下網頁:
-    // https://mitw.dicom.org.tw/IG/TWCR_SF/Condition-ConditionExample.json.html
     meta: {
       profile: [
         "https://mitw.dicom.org.tw/IG/TWCR/StructureDefinition/condition-profile"
@@ -22,7 +24,10 @@ module.exports.globalResource = {
     text: {
       status: "empty",
       div: "<div xmlns=\"http://www.w3.org/1999/xhtml\">目前為空值，可根據使用需求自行產生這筆資料的摘要資訊並填入此欄位</div>"
-    }
+    },
+    subject: {
+      reference: "Patient/PatientExample"
+    },
   }
 }
 
@@ -42,112 +47,142 @@ module.exports.fields = [
       // 在首個轉換項目檢查TWCR的package是否有更新
 
       let category = JSON.parse(`{
-        "coding":
-        {
-          "system": "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/class-of-case-codesystem",
-          "code": "codeValue",
-          "display": "displayValue"
-        }
+        "coding":[
+          {
+            "system": "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/class-of-case-codesystem",
+            "code": "codeValue",
+            "display": "displayValue"
+          }
+        ]
       }`);
-      // 此處的JSON String應避免帶有[]後續會比較好處理
 
-      category.coding.code = data;
-      category.coding.display = "申報醫院診斷，但未於申報醫院接受首次療程";
+      category.coding[0].code = data;
+      category.coding[0].display = "申報醫院診斷，但未於申報醫院接受首次療程";
       // diplay應抓取相對的ValueSet並從中找出code對應的顯示值
-
-      category.coding = [category.coding];// 把coding按照FHIR Definition包成Array
 
       return category;
       // ClassOfCase是category中的一個Slice
     }
   },
   {
-    source: 'ClassOfDiagnosisStatus',
+    source: 'CDS',
     target: 'Condition.category',
     beforeConvert: (data) => {
-      let category = data;
-      category.coding = [category.coding];// 把coding按照FHIR Definition包成Array
+      let category = JSON.parse(`
+      {
+        "coding" : [
+          {
+            "system" : "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/class-of-diangosis-status-codesystem",
+            "code" : "codeValue",
+            "display" : "displayValue"
+          }
+        ]
+      }
+      `);
+
+      category.coding[0].code = data;
+      category.coding[0].display = "於申報醫院診斷";
+      // diplay應抓取相對的ValueSet並從中找出code對應的顯示值
 
       return category;
       // ClassOfDiagnosisStatus是category中的一個Slice
     }
   },
   {
-    source: 'ClassOfTreatmentStatus',
+    source: 'CTS',
     target: 'Condition.category',
     beforeConvert: (data) => {
-      let category = data;
-      category.coding = [category.coding];// 把coding按照FHIR Definition包成Array
+      let category = JSON.parse(`
+      {
+        "coding" : [
+          {
+            "system" : "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/class-of-treatment-status-codesystem",
+            "code" : "codeValue",
+            "display" : "displayValue"
+          }
+        ]
+      }
+      `);
+
+      category.coding[0].code = data;
+      category.coding[0].display = "未於申報醫院接受任何治療即死亡";
+      // diplay應抓取相對的ValueSet並從中找出code對應的顯示值
 
       return category;
       // ClassOfTreatmentStatus是category中的一個Slice
     }
   },
   {
-    source: 'code',
+    source: 'SEQNO',
     target: 'Condition.code',
     beforeConvert: (data) => {
-      let code = data;
+      let code = JSON.parse(`
+      {
+        "coding" : [
+          {
+            "system" : "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/sequence-number-codesystem",
+            "code" : "codeValue",
+            "display" : "displayValue"
+          }
+        ]
+      }
+      `);
 
-      code.coding.code = parseInt(code.coding.code);
-      if (code.coding.code < 10) {
-        let cacheNumber = code.coding.code;
-        code.coding.code = "0" + String(cacheNumber);
+      code.coding[0].code = data;
+      code.coding[0].display = "個案一生中第 1 個惡性腫瘤或原位癌";
+      // diplay應抓取相對的ValueSet並從中找出code對應的顯示值
+
+      code.coding[0].code = parseInt(code.coding[0].code);
+      if (code.coding[0].code < 10) {
+        let cacheNumber = code.coding[0].code;
+        code.coding[0].code = "0" + String(cacheNumber);
         // 按照 https://mitw.dicom.org.tw/IG/TWCR_SF/ValueSet-sequence-number-valueset.html
         // 的定義值，把code value進行轉換
       }
-
-      code.coding = [code.coding];// 把coding按照FHIR Definition包成Array
 
       return code;
     }
   },
   {
-    source: 'subject',
-    target: 'Condition.subject'
-  },
-  { // optional
-    source: 'encounter',
-    target: 'Condition.encounter',
-    beforeConvert: (data) => {
-      if (data.reference == "") {
-        return null;
-      }
-      else
-        return data;
-    }
-  },
-  {
-    source: 'onsetAge',
+    source: 'DISAGE',
     target: 'Condition.onsetAge',
     beforeConvert: (data) => {
-      let onsetAge = data;
-      onsetAge.value = parseInt(onsetAge.value); // 根據FHIR的定義，應該要是正整數
-      return onsetAge
-    }
-  },
-  { // optional
-    source: 'recorder',
-    target: 'Condition.recorder',
-    beforeConvert: (data) => {
-      if (data.reference == "") {
-        return null;
+      let onsetAge = JSON.parse(`
+      {
+        "value" : 60,
+        "system" : "http://unitsofmeasure.org",
+        "code" : "a"
       }
-      else
-        return data;
+      `);
+      onsetAge.value = parseInt(data); // 根據FHIR的定義，應該要是正整數
+      return onsetAge;
     }
   },
   {
-    source: 'evidence',
+    source: 'DGNCON',
     target: 'Condition.evidence',
     beforeConvert: (data) => {
-      let evidence = data;
+      let evidence = JSON.parse(`
+      {
+        "code" : [
+          {
+            "coding" : [
+              {
+                "system" : "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/diagnostic-confirmation-codesystem",
+                "code" : "codeValue",
+                "display" : "displayValue"
+              }
+            ]
+          }
+        ]
+      }
+      `);
 
-      evidence.code.coding = [evidence.code.coding];// 把coding按照FHIR Definition包成Array
+      evidence.code[0].coding[0].code = data;
+      evidence.code[0].coding[0].display = "組織病理學確診(Positive histology)";
+      // diplay應抓取相對的ValueSet並從中找出code對應的顯示值
 
-      evidence.code = [evidence.code];// 把code按照mitw定義包成Array
       // https://mitw.dicom.org.tw/IG/TWCR_SF/StructureDefinition-condition-profile.html
-
       return evidence;
     }
   },
