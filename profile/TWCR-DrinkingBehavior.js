356ae7af6a906e974a58a77e372c6142c2416f98
郵差 <twHenryYang@gmail.com>
2023-02-12 20:55:04 +0800

    feat: Finish TWCR-DrinkingBehavior convert template

--- profile/TWCR-DrinkingBehavior.js
+++ profile/TWCR-DrinkingBehavior.js
@@ -11,19 +11,29 @@ module.exports.profile = {
 // 此Profile的JSON結構資料參考自以下網頁:
 // https://mitw.dicom.org.tw/IG/TWCR_SF/StructureDefinition-drinking-behavior-profile.html
 // 此Profile的完整JSON範例檔:
-// https://mitw.dicom.org.tw/IG/TWCR_SF/StructureDefinition-drinking-behavior-profile.profile.json.html
+// https://mitw.dicom.org.tw/IG/TWCR_SF/Observation-DrinkingBehaviorExample.json.html
 
 module.exports.globalResource = {
   // Should be resource name
   Observation: {
     meta: {
       profile: [
-        "profileURL"
+        "https://mitw.dicom.org.tw/IG/TWCR/StructureDefinition/drinking-behavior-profile"
       ]
     },
     text: {
       status: "empty",
       div: "<div xmlns=\"http://www.w3.org/1999/xhtml\">目前為空值，可根據使用需求自行產生這筆資料的摘要資訊並填入此欄位</div>"
+    },
+    status: "registered", //registered | preliminary | final | amended +
+    code: {
+      coding: [
+        {
+          system: "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/behavior-codesystem",
+          code: "Drinking",
+          display: "喝酒行為"
+        }
+      ]
     }
   }
 }
@@ -38,5 +48,25 @@ module.exports.fields = [
   },
   {
     // 喝酒行為	DRINKBE	valueCodeableConcept
+    source: 'DRINKBE',
+    target: 'Observation.valueCodeableConcept',
+    beforeConvert: (data) => {
+      let valueCodeableConcept = JSON.parse(`
+      {
+        "coding" : [
+          {
+            "system" : "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/drinking-behavior-codesystem",
+            "code" : "codeValue",
+            "display" : "displayValue"
+          }
+        ]
+      }
+      `);
+      valueCodeableConcept.coding[0].code = data;
+      let displayValue = tools.searchCodeSystemDisplayValue("../TWCR_ValueSets/definitionsJSON/CodeSystem-drinking-behavior-codesystem.json", data);
+      valueCodeableConcept.coding[0].display = displayValue;
+
+      return valueCodeableConcept;
+    }
   }
 ]
