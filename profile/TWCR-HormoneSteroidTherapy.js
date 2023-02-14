3a32ca5dd512b2803040f1dc7dac44eab8c393d0
郵差 <twHenryYang@gmail.com>
2023-02-14 17:37:37 +0800

    feat: Finish TWCR-HormoneSteroidTherapy convert template

--- profile/TWCR-HormoneSteroidTherapy.js
+++ profile/TWCR-HormoneSteroidTherapy.js
@@ -18,12 +18,22 @@ module.exports.globalResource = {
   Procedure: {
     meta: {
       profile: [
-        "profileURL"
+        "https://mitw.dicom.org.tw/IG/TWCR/StructureDefinition/hormone-steroid-therapy-profile"
       ]
     },
     text: {
       status: "empty",
       div: "<div xmlns=\"http://www.w3.org/1999/xhtml\">目前為空值，可根據使用需求自行產生這筆資料的摘要資訊並填入此欄位</div>"
+    },
+    status: "completed", //preparation | in-progress | not-done | on-hold | stopped | completed | entered-in-error | unknown
+    category: {
+      coding: [
+        {
+          system: "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/procedure-code-codesystem",
+          code: "HormoneSteroid",
+          display: "申報醫院荷爾蒙/類固醇治療"
+        }
+      ]
     }
   }
 }
@@ -38,6 +48,45 @@ module.exports.fields = [
   },
   {
     // 申報醫院荷爾蒙/類固醇治療	HOMSTTF	code
+    source: 'HOMSTTF',
+    target: 'Procedure.code',
+    beforeConvert: (data) => {
+      let code = JSON.parse(`
+      {
+        "coding" : [
+          {
+            "system" : "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/hormone-steroid-therapy-codesystem",
+            "code" : "code",
+            "display" : "display"
+          }
+        ]
+      }
+      `);
+      code.coding[0].code = data;
+      let displayValue = tools.searchCodeSystemDisplayValue("../TWCR_ValueSets/definitionsJSON/CodeSystem-hormone-steroid-therapy-codesystem.json", data);
+      code.coding[0].display = displayValue;
+
+      return code;
+    }
+  },
+  {
     // 申報醫院荷爾蒙/類固醇治療開始日期	DHSTSTF	performedPeriod.start
+    source: 'DHSTSTF',
+    target: 'Procedure.performedPeriod',
+    beforeConvert: (data) => {
+      let performedPeriod = JSON.parse(`
+      {
+        "start" : "2020-03-06"
+      }
+      `);
+      let s = String(data);
+      let YYYY = s[0] + s[1] + s[2] + s[3];
+      let MM = s[4] + s[5];
+      let DD = s[6] + s[7];
+
+      performedPeriod.start = `${YYYY}-${MM}-${DD}`;
+
+      return performedPeriod;
+    }
   }
 ]
