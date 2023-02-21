36bb71a4450f2c7f979da0d1f745ed98f8f1ad4c
郵差 <twHenryYang@gmail.com>
2023-02-21 14:44:46 +0800

    feat: Finish TWCR-RT convert template

--- profile/TWCR-RT.js
+++ profile/TWCR-RT.js
@@ -18,12 +18,25 @@ module.exports.globalResource = {
   Procedure: {
     meta: {
       profile: [
-        "profileURL"
+        "https://mitw.dicom.org.tw/IG/TWCR/StructureDefinition/rt-profile"
       ]
     },
     text: {
       status: "empty",
       div: "<div xmlns=\"http://www.w3.org/1999/xhtml\">目前為空值，可根據使用需求自行產生這筆資料的摘要資訊並填入此欄位</div>"
+    },
+    status: "completed", //preparation | in-progress | not-done | on-hold | stopped | completed | entered-in-error | unknown
+    category: {
+      coding: [
+        {
+          system: "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/procedure-code-codesystem",
+          code: "RT",
+          display: "申報醫院放射治療"
+        }
+      ]
+    },
+    subject: {
+      reference: "Patient/PatientExample"
     }
   }
 }
@@ -37,9 +50,46 @@ module.exports.fields = [
     }
   },
   {
-    // 放射治療開始日期	DORTST	performedPeriod.start
     // 放射治療機構	INSOFRT	code
-    
-    // 詢問Lily 放射治療機構值集CodeSystem疑似缺少code4
+    source: 'INSOFRT',
+    target: 'Procedure.code',
+    beforeConvert: (data) => {
+      let code = JSON.parse(`
+      {
+        "coding" : [
+          {
+            "system" : "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/institute-of-RT-codesystem",
+            "code" : "code",
+            "display" : "display"
+          }
+        ]
+      }
+      `);
+      code.coding[0].code = data;
+      let displayValue = tools.searchCodeSystemDisplayValue("../TWCR_ValueSets/definitionsJSON/CodeSystem-institute-of-RT-codesystem.json", data);
+      code.coding[0].display = displayValue;
+
+      return code;
+    }
+  },
+  {
+    // 放射治療開始日期	DORTST	performedPeriod.start
+    source: 'DORTST',
+    target: 'Procedure.performedPeriod',
+    beforeConvert: (data) => {
+      let performedPeriod = JSON.parse(`
+      {
+        "start" : "2020-03-06"
+      }
+      `);
+      let s = String(data);
+      let YYYY = s[0] + s[1] + s[2] + s[3];
+      let MM = s[4] + s[5];
+      let DD = s[6] + s[7];
+
+      performedPeriod.start = `${YYYY}-${MM}-${DD}`;
+
+      return performedPeriod;
+    }
   }
 ]
