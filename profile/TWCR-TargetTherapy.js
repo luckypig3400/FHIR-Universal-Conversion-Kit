0b6c40b74627a8e1fcb4dee7e562e1ca12fde8e8
郵差 <twHenryYang@gmail.com>
2023-02-23 14:53:33 +0800

    feat: Finish TWCR-TargetTherapy convert template

--- profile/TWCR-TargetTherapy.js
+++ profile/TWCR-TargetTherapy.js
@@ -18,12 +18,25 @@ module.exports.globalResource = {
   Procedure: {
     meta: {
       profile: [
-        "profileURL"
+        "https://mitw.dicom.org.tw/IG/TWCR/StructureDefinition/target-therapy-profile"
       ]
     },
     text: {
       status: "empty",
       div: "<div xmlns=\"http://www.w3.org/1999/xhtml\">目前為空值，可根據使用需求自行產生這筆資料的摘要資訊並填入此欄位</div>"
+    },
+    status: "completed", //preparation | in-progress | not-done | on-hold | stopped | completed | entered-in-error | unknown
+    category: {
+      coding: [
+        {
+          system: "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/procedure-code-codesystem",
+          code: "TargetedTherapy",
+          display: "申報醫院標靶治療"
+        }
+      ]
+    },
+    subject: {
+      reference: "Patient/PatientExample"
     }
   }
 }
@@ -38,6 +51,45 @@ module.exports.fields = [
   },
   {
     // 申報醫院標靶治療	TTTF	code
+    source: 'TTTF',
+    target: 'Procedure.code',
+    beforeConvert: (data) => {
+      let code = JSON.parse(`
+      {
+        "coding" : [
+          {
+            "system" : "https://mitw.dicom.org.tw/IG/TWCR/CodeSystem/targeted-therapy-codesystem",
+            "code" : "code",
+            "display" : "display"
+          }
+        ]
+      }
+      `);
+      code.coding[0].code = data;
+      let displayValue = tools.searchCodeSystemDisplayValue("../TWCR_ValueSets/definitionsJSON/CodeSystem-targeted-therapy-codesystem.json", data);
+      code.coding[0].display = displayValue;
+
+      return code;
+    }
+  },
+  {
     // 申報醫院標靶治療開始日期	DOTTSTF	performedPeriod.start
+    source: 'DOTTSTF',
+    target: 'Procedure.performedPeriod',
+    beforeConvert: (data) => {
+      let performedPeriod = JSON.parse(`
+      {
+        "start" : "2020-03-06"
+      }
+      `);
+      let s = String(data);
+      let YYYY = s[0] + s[1] + s[2] + s[3];
+      let MM = s[4] + s[5];
+      let DD = s[6] + s[7];
+
+      performedPeriod.start = `${YYYY}-${MM}-${DD}`;
+
+      return performedPeriod;
+    }
   }
 ]
